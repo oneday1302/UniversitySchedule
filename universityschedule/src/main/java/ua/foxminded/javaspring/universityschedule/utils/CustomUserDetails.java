8ca17bb7e7585273397f761c028020dfe570f66e
oneday1302 <kovalchukvladyslav2gmail.com>
2023-09-26 20:16:58 +0200

    some changed in CustomUserDetails class

--- universityschedule/src/main/java/ua/foxminded/javaspring/universityschedule/utils/CustomUserDetails.java
+++ universityschedule/src/main/java/ua/foxminded/javaspring/universityschedule/utils/CustomUserDetails.java
@@ -1,40 +1,32 @@
 package ua.foxminded.javaspring.universityschedule.utils;
 
 import lombok.EqualsAndHashCode;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import ua.foxminded.javaspring.universityschedule.entities.Role;
 import ua.foxminded.javaspring.universityschedule.entities.User;
 
 import java.util.Collection;
-import java.util.Set;
 
+@RequiredArgsConstructor
 @EqualsAndHashCode
 public class CustomUserDetails implements UserDetails {
 
-    private final String username;
-    private final String password;
-    private final Set<Role> roles;
-
-    public CustomUserDetails(User user) {
-        this.username = user.getUsername();
-        this.password = user.getPassword();
-        this.roles = user.getRoles();
-    }
+    private final User user;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return roles;
+        return user.getRoles();
     }
 
     @Override
     public String getPassword() {
-        return password;
+        return user.getPassword();
     }
 
     @Override
     public String getUsername() {
-        return username;
+        return user.getPassword();
     }
 
     @Override
